34a702012b55362604408cdbebeb34c58a79ce2e
WojciechTom <42958849+WojciechTom@users.noreply.github.com>
2018-09-23 11:48:39 +0200

    Update TriangleChecker.java

--- src/TriangleChecker.java
+++ src/TriangleChecker.java
@@ -2,7 +2,9 @@ public class TriangleChecker {
 
     boolean isRightTriangle (Triangle trojkat){
 
-        if( trojkat.getBokA()*trojkat.getBokA() + trojkat.getBokB()*trojkat.getBokB() == trojkat.getBokC()*trojkat.getBokC() ){
+        if( (trojkat.getBokA()*trojkat.getBokA() + trojkat.getBokB()*trojkat.getBokB() == trojkat.getBokC()*trojkat.getBokC()) ||
+            (trojkat.getBokB()*trojkat.getBokB() + trojkat.getBokC()*trojkat.getBokC() == trojkat.getBokA()*trojkat.getBokA()) || 
+            (trojkat.getBokC()*trojkat.getBokC() + trojkat.getBokA()*trojkat.getBokA() == trojkat.getBokB()*trojkat.getBokB()) ){
            return true;
         } else {
            return false;
